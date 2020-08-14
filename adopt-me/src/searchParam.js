20488825da589f0a79d82fd97645efc95e50dd5c
GenxTheGamer <vaibhavvyas32@gmail.com>
2020-08-14 11:15:16 +0530

    Files Updated | Custom Hooks

--- adopt-me/src/searchParam.js
+++ adopt-me/src/searchParam.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 //import React from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParam = () => {
   //const location = "Seattle, WA";
   const [location, setLocation] = useState("Seattle,US"); //You cannot use if else / for loop in hooks
-
+  //const [animal, setAnimal] = useState("dog");
+  //const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animals", "Dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
   return (
     <div className="search-params">
       <form>
@@ -19,6 +25,39 @@ const SearchParam = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        {/* <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlue={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label> */}
         <button>Submit</button>
       </form>
     </div>
